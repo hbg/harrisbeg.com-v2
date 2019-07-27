bc5d35f039ef490555412764bdb0911823a66d4f
Harris Beg <legostarwarsguy02@gmail.com>
2019-07-26 18:06:26 -0700

    Color Picker

--- harris/models.py
+++ harris/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from colorfield.fields import ColorField
 
 class Blog(models.Model):
     title = models.CharField(max_length=10)
     description = models.CharField(max_length=15, blank=True)
     content = models.TextField(max_length=1500)
     timestamp = models.DateTimeField()
+    color = ColorField(default='#FF0000')
 
     def __str__(self):
-        return self.content
+        return self.title
--- harris/settings.py
+++ harris/settings.py
@@ -35,8 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'harris'
+    'harris',
+    'colorfield',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
